8b5ce115d8ec70370227725e0fb8679cd876289b
adamggrover <93373074+adamggrover@users.noreply.github.com>
2024-05-01 22:27:47 +0100

    add posts container grid

---
+++ app/recipe/[slug]/page.js
@@ -0,0 +1,7 @@
+export default function recipePage(){
+    return(
+        <>
+            <h1>Hello World</h1>
+        </>
+    )
+}
---
+++ components/PostCard.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+export default function PostCard(props){
+    const {post} = props;
+    return(
+        <Link href={''} className="unstyled">
+            <div className="postCard">
+                <h3>{post.title}</h3>
+                <p>{post.bio}</p>
+                <div className="statsConatiner">
+                    <div>
+                        <h5>Prep Time</h5>
+                        <p>{post.prep_time}</p>
+                    </div>
+                    <div>
+                        <h5>Cook Time Time</h5>
+                        <p>{post.cook_time}</p>
+                    </div>
+                </div>
+
+            </div>
+        </Link>
+    )
+}
